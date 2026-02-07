c1b6f195d1a41334fbd19cba6a975df1a3e03438
Vladyslav Stasiuk <cheshire.cat.universe@gmail.com>
2026-02-07 16:02:42 +0200

    [Change] Back status code for tests

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -223,6 +223,7 @@ async def reset_password_complete(
 @router.post(
     "/login/",
     response_model=UserLoginResponseSchema,
+    status_code=status.HTTP_201_CREATED,
 )
 async def login_user(
     user_data: UserLoginRequestSchema,
